b9c193b3dc04cc9ae98417b944808416226261f0
Jakub Tucek <jakub.tucek@gmail.com>
2018-05-09 21:37:50 +0200

    Add default response type - in channel

--- app/controllers/CommandController.scala
+++ app/controllers/CommandController.scala
@@ -2,18 +2,23 @@ package controllers
 
 import domain._
 import javax.inject.{Inject, Singleton}
+import play.api.libs.json.{JsObject, JsString}
 import play.api.mvc.{AbstractController, Action, ControllerComponents}
 import service.{CommandService, MessagePostService}
 
 @Singleton
 class CommandController @Inject()(cc: ControllerComponents, commandService: CommandService, messagePostService: MessagePostService) extends AbstractController(cc) {
 
+  private lazy val defaultResponse = JsObject(Map(
+    "response_type" -> JsString("in_channel")
+  )
+  )
 
   def handleCommand: Action[Map[String, Seq[String]]] = Action(parse.formUrlEncoded) {
     request =>
       val cmd = InCommand.createFromMap(request.body)
       commandService.handleCommand(cmd) match {
-        case SuccessOutCommand() => Created
+        case SuccessOutCommand() => Created.apply(defaultResponse)
         case ErrorOutCommand(msg) =>
           messagePostService.postMessage(OutMessage.create(Attachment("Error!", msg, s"Error!: $msg :sweat:", "danger")), cmd.responseUrl)
           NoContent
